4b6bb8bb9412d175478e83afb6e4ead48eb016aa
MαηiFαsт <62830326+ManiFast@users.noreply.github.com>
2022-04-30 14:08:08 +0500

    Add files via upload

---
+++ NumbersCount/RandomGenerate.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <ctime>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	while (true)
+	{
+		double sim = rand() % 4;
+		double a, b;
+		a = rand() % 100;
+		b = rand() % 100;
+
+		cout << "\"" << a;
+		if (sim == 0)
+		{
+			cout << " + " << b << " = " << a + b;
+		}
+		else if (sim == 1)
+		{
+			cout << " - " << b << " = " << a - b;
+		}
+		else if (sim == 2)
+		{
+			cout << " * " << b << " = " << a * b;
+		}
+		else if (sim == 3)
+		{
+			cout << " / " << b << " = " << a / b;
+		}
+
+		cout << "\"" << endl;
+	}
+
+	system("pause");
+	return 0;
+}
